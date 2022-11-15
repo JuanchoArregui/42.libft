f286548f3be5afaf57947bf009ad76482a782968
jarregui <jarregui@student.42madrid.com>
2022-11-15 13:18:51 +0100

    fixed strncmp

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: jarregui <jarregui@student.42madrid.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/04/26 15:34:02 by jarregui          #+#    #+#             */
-/*   Updated: 2021/07/14 12:42:35 by jarregui         ###   ########.fr       */
+/*   Updated: 2022/11/15 13:13:40 by jarregui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,30 @@
 
 int	ft_strncmp(const char *s1, const char *s2, unsigned int n)
 {
-	unsigned int	i;
-
-	i = 0;
-	while (s1[i] != '\0' && s2[i] != '\0' && i < n)
+	while (*s1 && (*s1 == *s2) && n > 0)
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
-		++i;
+		s1++;
+		s2++;
+		n--;
 	}
-	if (i != n)
-		return (s1[i] - s2[i]);
-	return (0);
+	if (n == 0)
+		return (0);
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
+
+/*
+DESCRIPTION
+     The strcmp() and strncmp() functions lexicographically compare the null-terminated strings s1 and s2.
+
+     The strncmp() function compares not more than n characters.  Because strncmp() is designed for comparing strings rather than binary data, characters
+     that appear after a `\0' character are not compared.
+
+RETURN VALUES
+     The strcmp() and strncmp() functions return an integer greater than, equal to, or less than 0, according as the string s1 is greater than, equal to,
+     or less than the string s2.  The comparison is done using unsigned characters, so that `\200' is greater than `\0'.
+
+
+
+
+	
+	*/
